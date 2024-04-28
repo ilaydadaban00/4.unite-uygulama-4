d50d46791fc598208d5524c8ee4748413b76e654
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-04-28 17:48:04 +0300

    Add files via upload

---
+++ 4.unite uygulamaa4/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace _4.unite_uygulamaa4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtDersNotu = new System.Windows.Forms.TextBox();
+            this.txtEnYuksek = new System.Windows.Forms.TextBox();
+            this.txtEnDusuk = new System.Windows.Forms.TextBox();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ad soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ders notu";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(137, 28);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 2;
+            // 
+            // txtDersNotu
+            // 
+            this.txtDersNotu.Location = new System.Drawing.Point(137, 82);
+            this.txtDersNotu.Name = "txtDersNotu";
+            this.txtDersNotu.Size = new System.Drawing.Size(100, 20);
+            this.txtDersNotu.TabIndex = 3;
+            // 
+            // txtEnYuksek
+            // 
+            this.txtEnYuksek.Location = new System.Drawing.Point(119, 278);
+            this.txtEnYuksek.Name = "txtEnYuksek";
+            this.txtEnYuksek.Size = new System.Drawing.Size(100, 20);
+            this.txtEnYuksek.TabIndex = 4;
+            // 
+            // txtEnDusuk
+            // 
+            this.txtEnDusuk.Location = new System.Drawing.Point(119, 331);
+            this.txtEnDusuk.Name = "txtEnDusuk";
+            this.txtEnDusuk.Size = new System.Drawing.Size(100, 20);
+            this.txtEnDusuk.TabIndex = 5;
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Location = new System.Drawing.Point(119, 382);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(100, 20);
+            this.txtOrtalama.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 217);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 7;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "en yuksek";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "en dusuk ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 382);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "ortalama";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(438, 382);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 11;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Location = new System.Drawing.Point(383, 178);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(178, 108);
+            this.lbListe.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.txtEnDusuk);
+            this.Controls.Add(this.txtEnYuksek);
+            this.Controls.Add(this.txtDersNotu);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtDersNotu;
+        private System.Windows.Forms.TextBox txtEnYuksek;
+        private System.Windows.Forms.TextBox txtEnDusuk;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.ListBox lbListe;
+    }
+}
+
---
+++ 4.unite uygulamaa4/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.unite_uygulamaa4
+{
+    public partial class Form1 : Form
+    {
+        string[] isimler = new string[5]; //Global dizi
+        int[] notlar = new int[5]; //Global dizi
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int enyuksek = notlar[0];
+            for(int i = 0; i <= notlar.Length; i++)
+            {
+                if (notlar[i] > enyuksek)
+                {
+                    enyuksek = notlar[i];
+                }  
+            }
+            txtEnYuksek.Text = enyuksek.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (index < isimler.Length)
+            {
+                isimler[index] = txtAdSoyad.Text;
+                notlar.  [index] = int.Parse(txtDersNotu.Text);
+                // int.Parse fonksiyonu girilen değeri “int” veri türüne dönüştürür
+                index++;
+                txtAdSoyad.Text = "";
+                txtDersNotu.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int endusuk = notlar[0];
+            for (int i = 0; i <= notlar.Length; i++)
+            {
+                if (notlar[i] < endusuk)
+                {
+                    endusuk = notlar[i];
+                }
+            }
+            txtEnDusuk.Text = endusuk.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            double ortalama = 0;
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplam += notlar[i];
+            }
+            ortalama = toplam / notlar.Length;
+            txtOrtalama.Text = ortalama.ToString();
+        }
+    }
+}
